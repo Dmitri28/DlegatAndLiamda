3266fba6ca19c888dccbca6142e3ab6173470f49
Dmitri28 <117075948+Dmitri28@users.noreply.github.com>
2023-01-29 17:13:41 +0200

    ProjectAboutFunction
    ProjectAboutFunction

---
+++ Liam2.cs/Info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liam2.cs
+{
+    class Info
+    {
+        private string firstName;
+        private string lastName;
+        public void setFirstName(string firstName) => this.firstName=firstName;
+        public void setLastName(string lastName) => this.lastName = lastName;
+      public string getLastName()
+        {
+            return lastName;
+        }
+        public string getFirstName()
+        {
+            return firstName;
+        }
+            }
+}
---
+++ Liam2.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Liam2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info info = new Info();
+            info.setFirstName("Jim");
+            info.setLastName("Bim");
+            Console.WriteLine(info.getFirstName() + " " + info.getLastName());
+        }
+    }
+}
